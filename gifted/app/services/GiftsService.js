170c1fdd28ab40373bb8eeb05e2cdbe3f0826aa5
Michael Giesler <michael.giesler1@icloud.com>
2024-11-29 15:07:10 -0700

    set up gifts service with api integration

--- gifted/app/services/GiftsService.js
+++ gifted/app/services/GiftsService.js
@@ -9,3 +9,36 @@ const giphy = axios.create({
     api_key: 'GCekFU9TP23Rr6ZtP1WsX1fN8j1hNKQy'
   }
 })
+
+class GiftsService {
+  async getGifts() {
+    const res = await api.get('api/gifts')
+    AppState.gifts = res.data.map(g => new Gift(g))
+  }
+
+  async createGift(giftData) {
+    const res = await api.post('api/gifts', giftData)
+    const gift = new Gift(res.data)
+    AppState.gifts.unshift(gift)
+  }
+
+  async openGift(giftId) {
+    const res = await api.put(`api/gifts/${giftId}`, { opened: true })
+    const giftIndex = AppState.gifts.findIndex(g => g.id == giftId)
+    if (giftIndex !== -1) {
+      AppState.gifts.splice(giftIndex, 1, new Gift(res.data))
+    }
+  }
+
+  async searchGifs(query) {
+    const res = await giphy.get('search', {
+      params: {
+        q: query,
+        limit: 24
+      }
+    })
+    AppState.giphyResults = res.data.data.map(g => g.images.downsized.url)
+  }
+}
+
+export const giftsService = new GiftsService()
