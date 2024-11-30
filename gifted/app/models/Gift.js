4bb76ee67c1da409117c556f057609e128056185
Michael Giesler <michael.giesler1@icloud.com>
2024-11-30 12:40:07 -0700

    made changes to gift model to properly handle gifts controller.

--- gifted/app/models/Gift.js
+++ gifted/app/models/Gift.js
@@ -13,7 +13,7 @@ export class Gift {
   get GiftCardTemplate() {
     return /*html*/`
     <div class="col-md-3 p-3">
-      <div class="gift-card ${this.opened ? 'opened' : ''}" onclick="app.GiftsControlller.openGift('${this.id}')">
+      <div class="gift-card ${this.opened ? 'opened' : ''}" ${this.opened ? '' : `onclick="app.GiftsController.openGift('${this.id}')"`}>
         <div class="gift-image" style="background-image: url(${this.opened ? this.url : 'assets/img/gift-box.png'})">
           ${!this.opened ? '<div class="click-to-open">Click to Open</div>' : ''}
         </div>
