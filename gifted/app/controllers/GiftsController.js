74b8c78c1ffdf4884fa492498bd37d2fa1076873
Michael Giesler <michael.giesler1@icloud.com>
2024-11-29 20:08:50 -0700

    added routing logic to gifts controller

--- gifted/app/controllers/GiftsController.js
+++ gifted/app/controllers/GiftsController.js
@@ -1,1 +1,81 @@
-import { AppState } from "";
+import { AppState } from "../AppState.js"
+import { giftsService } from "../services/GiftsService.js"
+import { getFormData } from "../utils/FormHandler.js"
+import { Pop } from "../utils/Pop.js"
+import { setHTML } from "../utils/Writer.js"
+
+function _drawGifts() {
+  let template = ''
+  AppState.gifts.forEach(gift => template += gift.GiftCardTemplate)
+  setHTML('giftsContainer, template')
+}
+
+function _drawGiphyResults() {
+  let template = ''
+  AppState.giphyResults.forEach(url => {
+    template += `
+      <div class="col-4 p-2">
+        <img 
+          src="${url}"
+          alt="gif option"
+          class="selectable gif-result"
+          onclick="app.GiftsController.selectGif('${url}')"
+        >
+      </div>
+    `
+  })
+  setHTML('giphyResults', template)
+}
+
+export class GiftsController {
+  constructor() {
+    AppState.on('gifts', _drawGifts)
+    AppState.on('giphyResults', _drawGiphyResults)
+    AppState.on('account, this.getGifts')
+    this.getGifts()
+  }
+
+  async getGifts() {
+    try {
+      await giftsService.getGifts()
+    } catch (error) {
+      Pop.error(error)
+    }
+  }
+
+  async createGift(event) {
+    try {
+      event.preventDefault()
+      const form = event.target
+      const giftData = getFormData(form)
+      await giftsService.createGift(giftData)
+      form.reset()
+      // @ts-ignore
+      bootstrap.Offcanvas.getInstance('#giftForm').hide()
+    } catch (error) {
+      Pop.error(error)
+    }
+  }
+
+  async openGift(giftId) {
+    try {
+      await giftsService.openGift(giftId)
+    } catch (error) {
+      Pop.error(error)
+    }
+  }
+
+  async searchGifs() {
+    try {
+      const searchTerm = document.getElementById('giphySearch').value
+      if (!searchTerm) { return }
+      await giftsService.searchGifs(searchTerm)
+    } catch (error) {
+      Pop.error(error)
+    }
+  }
+
+  selectGif(url) {
+    document.getElementById('giftUrl').value = url
+  }
+}
